b9803925c67ab32f6d3bec59c2e5a0be08c4c776
sjony <jshu1@xiaohongshu.com>
2018-02-23 19:53:39 +0800

    DUBBO服务

---
+++ order-module/src/main/java/com/sjony/study/sort/MyHeapSort.java
@@ -0,0 +1,38 @@
+package com.sjony.study.sort;
+
+/**
+ * Created by sjony on 2018/2/23.
+ */
+public class MyHeapSort {
+
+    public static void sort(int[] arr) {
+
+    }
+
+    public static void adjustHeap(int[] arr, int i, int length) {
+
+        //当前节点数
+        int temp = arr[i];
+
+        //比较节点子节点数
+        for()
+
+    }
+
+    /**
+     * @Description:交换数组
+     * @Create on: 2018/2/23 下午7:47
+     * @author jshu
+     *
+     * @param arr
+     * @param i
+     * @param j
+     *
+     */
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}
